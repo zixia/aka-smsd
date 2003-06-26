c3c62c9d4da5b6b2fb6bf67308ef9152dddca18f
Hua QU <royqh1979@gmail.com>
2003-06-26 03:47:36 +0000

    login bug

--- smsbbschildprotocol.h
+++ smsbbschildprotocol.h
@@ -468,12 +468,10 @@ redo0:
 	}
 	msgType=(PSMS_BBS_HEADER(buf))->Type;
 
-
-	DWORD smsSerialNo, msgLen , pid;
+#ifdef DEBUG
+	DWORD smsSerialNo, msgLen ;
 	msgLen=sms_byteToLong((PSMS_BBS_HEADER(buf))->msgLength);
 	smsSerialNo=sms_byteToLong((PSMS_BBS_HEADER(buf))->SerialNo);
-	pid=sms_byteToLong((PSMS_BBS_HEADER(buf))->pid);
-#ifdef DEBUG
 	syslog(LOG_ERR,"login msg head length %d",msgLen);
 	syslog(LOG_ERR,"login msg sn %d",smsSerialNo);
 #endif
@@ -493,11 +491,11 @@ redo02:
 	}
 	
 	if (!m_pChildPrivilegeChecker->canUserConnect( (PSMS_BBS_LOGINPACKET(buf))->user,(PSMS_BBS_LOGINPACKET(buf))->password)  ){
-		doReply(SMS_BBS_CMD_ERR,smsSerialNo,pid);
+		doReply(SMS_BBS_CMD_ERR,(PSMS_BBS_HEADER(buf))->SerialNo,(PSMS_BBS_HEADER(buf))->pid);
 		syslog(LOG_ERR,"connection user & password wrong!");
 		return -1;
 	}
-	doReply(SMS_BBS_CMD_OK,smsSerialNo,pid);
+	doReply(SMS_BBS_CMD_OK,(PSMS_BBS_HEADER(buf))->SerialNo,(PSMS_BBS_HEADER(buf))->pid);
 
 	m_pStream=pStream;
 
@@ -740,7 +738,7 @@ public:
 			syslog(LOG_ERR," connect DB error: %s",er.error.c_str());
 			exit(-1);
 		}
-		m_pChildPrivilegeChecker=new CSMSBBSChildPrivilegeChecker(childCode, password,addr,m_conn);
+		m_pChildPrivilegeChecker=new CSMSBBSChildPrivilegeChecker(childCode, password,addr,&m_conn);
 		strncpy(m_childCode,childCode,SMS_CHILDCODE_LEN);
 		m_listenPort=port;
 	}
