06b46da22b8ca5d754065c32d627813cdc7aa405
Hua QU <royqh1979@gmail.com>
2003-07-09 01:33:18 +0000

    zixia.net bbsd 改动

--- zixianetbbssmsd.cpp
+++ zixianetbbssmsd.cpp
@@ -9,9 +9,9 @@ public:
 	
 
 	CMyDaemon(char* applicationName,int logFacility): CSMSDaemon(applicationName,logFacility){
-		m_pSMSProtocol=new CSMSBBSChildProtocol("13","zixiabbs","202.112.80.3",4003);
+		m_pSMSProtocol=new CSMSBBSChildProtocol("13","zixiabbs","202.205.10.10",4003);
 		m_pSMSStorage = new CSMSDiskStorage(m_pSMSProtocol,"/home/roy/gateway/in","/home/roy/gateway/zixiabbsgo");
 	}
 };
 
-CMyDaemon myDaemon("testbbschild",LOG_LOCAL0);
+CMyDaemon myDaemon("zixia.net.bbschild",LOG_LOCAL0);
