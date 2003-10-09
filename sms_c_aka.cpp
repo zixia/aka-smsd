33e2668b6af6c2728507bd71e394151e5d64823f
Hua QU <royqh1979@gmail.com>
2003-10-09 07:31:17 +0000

    修改

--- sms_c_aka.cpp
+++ sms_c_aka.cpp
@@ -12,8 +12,8 @@ public:
 
 	CMyDaemon(char* applicationName,int logFacility): CSMSDaemon(applicationName,logFacility){
 		m_pSMSProtocol=new CSMSChildProtocol;
-		m_pSMSStorage = new CSMSDiskStorage(m_pSMSProtocol,SMSHOME "outbox/deliver", SMSHOME "inbox/default", SMSHOME "inbox.back/default");
+		m_pSMSStorage = new CSMSDiskStorage(m_pSMSProtocol,SMSHOME "outbox/deliver", SMSHOME "inbox/c_aka", SMSHOME "inbox.back/c_aka");
 	}
 };
 
-CMyDaemon myDaemon("testchild",LOG_LOCAL0);
+CMyDaemon myDaemon("sms_c_aka",LOG_LOCAL0);
